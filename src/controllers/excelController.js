1aaea30a8cf3ae70381a38c3dee4bf1826a74062
TomasSorgetti <97346262+TomasSorgetti@users.noreply.github.com>
2024-04-15 02:16:50 -0300

    excel filtred working!!

--- src/controllers/excelController.js
+++ src/controllers/excelController.js
@@ -1,41 +1,40 @@
 const { payment } = require("../db");
-const excelJS = require("exceljs");
 
-const exportToExcelController = async (req, res) => {
-  const { id } = req.user;
-  if (!id) {
-    throw new Error("Unauthorized");
+const { Op } = require("sequelize");
+
+const getExcelList = async (
+  id,
+  name,
+  order,
+  orderBy,
+  filter,
+  minAmount,
+  maxAmount,
+  minDate,
+  maxDate
+) => {
+  let whereClause = {
+    userId: id,
+    addressee: {
+      [Op.like]: name + "%",
+    },
+  };
+  if (!minAmount) minAmount = 0;
+  if (maxAmount) whereClause.amount = { [Op.between]: [minAmount, maxAmount] };
+  if (minDate && maxDate) {
+    whereClause.paymentDate = { [Op.between]: [minDate, maxDate] };
+  } else if (minDate && !maxDate) {
+    whereClause.paymentDate = { [Op.gte]: minDate };
+  } else if (maxDate && !minDate) {
+    whereClause.paymentDate = { [Op.lte]: maxDate };
   }
-  let workbook = new excelJS.Workbook();
-  const sheet = workbook.addWorksheet("payments");
-  sheet.columns = [
-    { header: "amount", key: "amount", width: 35 },
-    { header: "paymentType", key: "paymentType", width: 35 },
-    { header: "addressee", key: "addressee", width: 35 },
-    { header: "paymentDate", key: "paymentDate", width: 35 },
-  ];
-
-  const payments = await payment.findAll({ where: { userId: id } });
-
-  payments.forEach((payment) => {
-    sheet.addRow({
-      amount: payment.amount,
-      paymentType: payment.paymentType,
-      addressee: payment.addressee,
-      paymentDate: payment.paymentDate,
-    });
+  if (filter && ["check", "debit", "transfer", "credit"].includes(filter)) {
+    whereClause.paymentType = filter;
+  }
+  return await payment.findAll({
+    where: whereClause,
+    order: [[orderBy, order]],
   });
-
-  res.setHeader(
-    "Content-Type",
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-  );
-  res.setHeader(
-    "Content-Disposition",
-    "attachment; filename=payment_list_blaze.xlsx"
-  );
-  await workbook.xlsx.write(res);
-  res.end();
 };
 
-module.exports = { exportToExcelController };
+module.exports = { getExcelList };
--- src/handlers/excelHandler.js
+++ src/handlers/excelHandler.js
@@ -1,9 +1,62 @@
-const { exportToExcelController } = require("../controllers/excelController");
+const { getExcelList } = require("../controllers/excelController");
+const excelJS = require("exceljs");
 
 const getExcelHandler = async (req, res) => {
+  const { id } = req.user;
+  if (!id) {
+    res.status(403).json({ msg: "Unauthorized" });
+  }
+  const {
+    name,
+    order,
+    orderBy,
+    filter,
+    minAmount,
+    maxAmount,
+    minDate,
+    maxDate,
+  } = req.query;
   try {
-    const response = await exportToExcelController(req, res);
-    res.status(200).json(response);
+    const response = await getExcelList(
+      id,
+      name,
+      order,
+      orderBy,
+      filter,
+      minAmount,
+      maxAmount,
+      minDate,
+      maxDate
+    );
+    let workbook = new excelJS.Workbook();
+    const sheet = workbook.addWorksheet("payments");
+    sheet.columns = [
+      { header: "amount", key: "amount", width: 35 },
+      { header: "paymentType", key: "paymentType", width: 35 },
+      { header: "addressee", key: "addressee", width: 35 },
+      { header: "paymentDate", key: "paymentDate", width: 35 },
+    ];
+
+    response.forEach((payment) => {
+      sheet.addRow({
+        amount: payment.amount,
+        paymentType: payment.paymentType,
+        addressee: payment.addressee,
+        paymentDate: payment.paymentDate,
+      });
+    });
+
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename=payment_list_blaze.xlsx"
+    );
+    await workbook.xlsx.write(res);
+    res.end();
+    // res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
